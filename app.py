58bb2296856060712e7893bbfab5bbb5f7036944
J. S. Lin <chunhsien.lin@gmail.com>
2024-05-28 17:38:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def handle_message(event):
 @app.route("/healthz", methods=['GET'])
 def healthz():
     app.logger.info("Health trigger")
-    return 'OK'
+    return '200 OK'
 
 
 if __name__ == "__main__":
